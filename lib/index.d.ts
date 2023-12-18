40f35ae732d002fc34d5f1c15c8d04344951b7cb
Axel Ternisien <axel.ternisien@hotmail.com>
2023-12-18 11:53:28 +0100

    Update index.d.ts

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,6 +1,6 @@
 import _Vue from "vue";
 import { SweetAlertOptions, SweetAlertResult, SweetAlertType } from "sweetalert2";
-export declare class VueSimpleAlert {
+export declare class VueSimpleAlertModify {
     static globalOptions: SweetAlertOptions;
     static alert(message?: string, title?: string, type?: SweetAlertType, options?: SweetAlertOptions): Promise<boolean>;
     static confirm(message?: string, title?: string, type?: SweetAlertType, options?: SweetAlertOptions): Promise<boolean>;
@@ -10,16 +10,16 @@ export declare class VueSimpleAlert {
 }
 declare module "vue/types/vue" {
     interface Vue {
-        $alert: typeof VueSimpleAlert.alert;
-        $confirm: typeof VueSimpleAlert.confirm;
-        $prompt: typeof VueSimpleAlert.prompt;
-        $fire: typeof VueSimpleAlert.fire;
+        $alert: typeof VueSimpleAlertModify.alert;
+        $confirm: typeof VueSimpleAlertModify.confirm;
+        $prompt: typeof VueSimpleAlertModify.prompt;
+        $fire: typeof VueSimpleAlertModify.fire;
     }
     interface VueConstructor {
-        alert: typeof VueSimpleAlert.alert;
-        confirm: typeof VueSimpleAlert.confirm;
-        prompt: typeof VueSimpleAlert.prompt;
-        fire: typeof VueSimpleAlert.fire;
+        alert: typeof VueSimpleAlertModify.alert;
+        confirm: typeof VueSimpleAlertModify.confirm;
+        prompt: typeof VueSimpleAlertModify.prompt;
+        fire: typeof VueSimpleAlertModify.fire;
     }
 }
-export default VueSimpleAlert;
+export default VueSimpleAlertModify;
